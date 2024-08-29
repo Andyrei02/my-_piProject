fbb31fe0e574983962ba87552d0aa98a35a45bcd
Andrei <69972869+Andyrei02@users.noreply.github.com>
2024-08-29 12:54:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import sys
+import os
+import time
+
+import tts
+
+
+def main():
+  while True:
+    text = input("> ")
+    tts.va_speak(text)
+    if text == 'exit':
+      break
+
+if __name__ == '__main__':
+  main()
